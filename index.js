39a0cd13252ebf7cc44d6a47d775e46e972bf7ee
Alif102 <alifahmed102@gmail.com>
2023-11-09 13:43:15 +0600

    get bookings email

--- index.js
+++ index.js
@@ -73,6 +73,14 @@ app.get("/bookings/:id", async(req,res)=> {
   res.send(result)
 })
 
+app.get('/bookings/:email', async (req, res) => {
+  const email = req.params.email  
+  const result = await bookingCollection.find({ email: email }).toArray()
+  res.send(result)
+
+})
+
+
 
 
   
