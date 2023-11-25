25e8618ee4a47892900cf763850d67f472376efe
Alif102 <alifahmed102@gmail.com>
2023-11-25 12:02:58 +0600

    create userCollection

--- index.js
+++ index.js
@@ -3,19 +3,24 @@ const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const cors = require('cors');
 const app = express(); 
+const jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser');
 const port = process.env.PORT || 5000;
 
 // middleware
-
+// https://backend-nine-liart.vercel.app/ http://localhost:5173/
  app.use(cors({
   origin : [
-    'https://backend-nine-liart.vercel.app',
+     'http://localhost:5173', 'https://hotel-booking-mern-c8edc.web.app' , 'https://hotel-booking-mern-c8edc.firebaseapp.com'
   ],
   credentials: true
  }));
  app.use(express.json());
-
  app.use(cookieParser());
+
+//  app.use(cookieParser());
+
+
 
 
 
@@ -32,20 +37,39 @@ const client = new MongoClient(uri, {
   });
 
 
-
-  const verifyToken = async (req, res, next) => {
-    const token = req.cookies?.token;
-    if (!token) {
-        return res.status(401).send({ message: 'unauthorized access' })
-    }
-    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
-        if (err) {
-            return res.status(401).send({ message: 'unauthorized access' })
-        }
-        req.user = decoded;
-        next();
-    })
+const logger = (req, res, next)=> {
+  console.log('log : info', req.method, req.url);
+  next();
 }
+const verifyToken = (req,res,next) => {
+  const token = req?.cookies?.token;
+  if(!token){
+    return res.status(401).send({message : 'unauthorize access'})
+  }
+  jwt.verify(token,process.env.SECRET_TOKEN, (err,decoded)=> {
+    if(err){
+      return res.status(401).send({message: 'unauthorized access'})
+    }
+    req.user = decoded;
+    next();
+
+  })
+  console.log('middleware', token);
+
+}
+//   const verifyToken = async (req, res, next) => {
+//     const token = req.cookies?.token;
+//     if (!token) {
+//         return res.status(401).send({ message: 'unauthorized access' })
+//     }
+//     jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+//         if (err) {
+//             return res.status(401).send({ message: 'unauthorized access' })
+//         }
+//         req.user = decoded;
+//         next();
+//     })
+// }
   async function run() {
     try {
       // Connect the client to the server	(optional starting in v4.7)
@@ -55,9 +79,29 @@ const client = new MongoClient(uri, {
 
       
        const bookingCollection = client.db('Booking').collection('bookings')
+       const usersCollection = client.db('Booking').collection('user')
 
       // const bookingCollection = client.db('Booking').collection('bookings');
       const roomCollection = client.db('Booking').collection('rooms')
+    app.post('/user',async(req,res)=> {
+      const user = req.body;
+      console.log(user)
+      const result = await usersCollection.insertOne(user);
+      res.send(result)
+    })
+    app.get('/user', async (req,res)=> {
+      const cursor = usersCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    })
+    app.get("/user/:id", async(req,res)=> {
+      const id = req.params.id;
+      // const smallBrandName = brandName.toLowerCase()
+      const query = {_id :new ObjectId (id)};
+      const result = await usersCollection.findOne(query);
+      res.send(result)
+    })
+
       app.post('/rooms', async(req,res)=> {
         const newProduct = req.body;
         console.log(newProduct)
@@ -85,8 +129,11 @@ const client = new MongoClient(uri, {
     res.send(result)
 })
 
-app.get('/bookings', async (req,res)=> {
-  const id = req.params.id
+app.get('/bookings', logger, verifyToken, async (req,res)=> {
+  const id = req.params.id;
+  // console.log('cooockies', req.cookies); 
+
+
   console.log(id)
   const cursor = bookingCollection.find();
   const result = await cursor.toArray();
@@ -101,24 +148,49 @@ app.get('/bookings', async (req,res)=> {
 //   res.send(result)
 // })
 
-app.get('/bookings/:email', async (req, res) => {
-  const email = req.params.email  
+app.get('/bookings/:email', logger, verifyToken, async (req, res) => {
+  const email = req.params.email ;
   const result = await bookingCollection.find({ email: email }).toArray()
   res.send(result)
 
 })
 
-app.post('/jwt', async (req,res)=> {
+app.post('/jwt', logger, verifyToken, async (req,res)=> {
   const user =req.body;
   console.log(user);
   const token = jwt.sign(user, process.env.SECRET_TOKEN, {expiresIn: '1h'});
-  res
-                .cookie('token', token, {
+  res.cookie('token', token, {
                     httpOnly: true,
-                    secure: false
+                    secure: true,
+                    sameSite: 'none'
                 })
                 .send({ success: true })
         })
+
+        app.post('/logout', async(req,res)=>{
+          const user = req.body;
+          console.log('logging out', user)
+          res.clearCookie('token', {maxAge : 0, secure:true}).send({success: true})
+        })
+
+         // Save or modify user email, status in DB
+    // app.put('/users/:email', async (req, res) => {
+    //   const email = req.params.email
+    //   const user = req.body
+    //   const query = { email: email }
+    //   const options = { upsert: true }
+    //   const isExist = await bookingCollection.findOne(query)
+    //   console.log('User found?----->', isExist)
+    //   if (isExist) return res.send(isExist)
+    //   const result = await bookingCollection.updateOne(
+    //     query,
+    //     {
+    //       $set: { ...user, timestamp: Date.now() },
+    //     },
+    //     options
+    //   )
+    //   res.send(result)
+    // })
 
 
 
